243ea08c1c3069098b58f2d4ee93d6e356fcb485
George Prountzos <g.prountzos@vieconnect.io>
2024-09-30 21:45:19 +0300

    add init controller

---
+++ src/main/java/com/intrasoft/benef1/Controller.java
@@ -0,0 +1,29 @@
+package com.intrasoft.benef1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/v1/")
+
+public class Controller {
+
+  @GetMapping("/ping")
+  public String ping() throws IOException {
+    return "pong";
+  }
+
+  @GetMapping("/get")
+  public String home() {
+    return "This is the get request";
+  }
+
+  @GetMapping("/get/check")
+  public String home1() {
+
+    return "This is the get check request";
+  }
+}
