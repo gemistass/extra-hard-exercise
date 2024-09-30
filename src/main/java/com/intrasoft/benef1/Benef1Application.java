8fed16f69c7e871ce45a8a57969657dac5bb3011
George Prountzos <g.prountzos@vieconnect.io>
2024-09-30 21:33:20 +0300

    initial commit

---
+++ src/main/java/com/intrasoft/benef1/Benef1Application.java
@@ -0,0 +1,13 @@
+package com.intrasoft.benef1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Benef1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Benef1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/intrasoft/benef1/Benef1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intrasoft.benef1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Benef1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
